54087aeb21afe1d0df5862ba7b2c4be66b6b513f
megan <msmeganmcmanus@gmail.com>
2021-02-13 08:26:52 -0800

    add first lesson

---
+++ 03_layout/App.js
@@ -0,0 +1,13 @@
+const App = () => (
+    <div>
+      <h2>Dogs!</h2>
+      <Shiba />
+      <SharPei />
+      <Shiba />
+    </div>
+  )
+  
+  
+  
+  ReactDOM.render(<App />, document.getElementById("root"))
+  
---
+++ 05_jsxAndProps/RandomNumRange.js
@@ -0,0 +1,9 @@
+const RandomNumRange = ({ min = 1, max = 10 }) => {
+    const rand = Math.floor(Math.random() * (max - min)) + min;
+    return <h1>Rand Is: {rand}</h1>
+  }
+  
+  
+  
+  
+  
